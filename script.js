e7a9661db6471a692794f612a90b4168bf0458fc
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-07-25 12:12:24 +0200

    remote save setup

---
+++ script.js
@@ -0,0 +1,6 @@
+const STORAGE_TOKEN = "ZDBRU8MO3ITOBEFYBL1UZM7T4PEOJJT3PRTJG4C5";
+const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
+
+async function setItem(key, value) {}
+
+async function getItem(key) {}
