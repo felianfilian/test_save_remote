1976b3efae1a18e6b5f0183e6492ff511bd5815a
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-07-26 00:02:09 +0200

    added then()

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
 
 async function setItem(key, value) {
   const payload = { key, value, token: STORAGE_TOKEN };
-  return fetch(STORAGE_URL, { method: "POST", body: JSON.stringify(payload) });
+  return fetch(STORAGE_URL, {
+    method: "POST",
+    body: JSON.stringify(payload),
+  }).then((res) => res.json());
 }
 
 async function getItem(key) {
   const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
   debugger;
-  return fetch(url);
+  return fetch(url).then((res) => res.json);
 }
