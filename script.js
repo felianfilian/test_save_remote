92e3047a53ca8001a381955922449a685432246b
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-07-25 23:06:15 +0200

    GET test

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
 
 async function setItem(key, value) {
   const payload = { key, value, token: STORAGE_TOKEN };
-  return fetch(STORAGE_URLm, { method: "POST", body: JSON.stringify(payload) });
+  return fetch(STORAGE_URL, { method: "POST", body: JSON.stringify(payload) });
 }
 
-async function getItem(key) {}
+async function getItem(key) {
+  const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+  debugger;
+  return fetch(url);
+}
