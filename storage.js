eac0f36bbb4a80274c29d76bb0d21aa93cbc1c33
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-07-26 00:27:56 +0200

    new file structure

---
+++ storage.js
@@ -0,0 +1,16 @@
+const STORAGE_TOKEN = "ZDBRU8MO3ITOBEFYBL1UZM7T4PEOJJT3PRTJG4C5";
+const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
+
+async function setItem(key, value) {
+  const payload = { key, value, token: STORAGE_TOKEN };
+  return fetch(STORAGE_URL, {
+    method: "POST",
+    body: JSON.stringify(payload),
+  }).then((res) => res.json());
+}
+
+async function getItem(key) {
+  const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+  debugger;
+  return fetch(url).then((res) => res.json);
+}
